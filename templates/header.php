940adb160b427333e192ffd27aa09c59a7ebca98
Jessy Charlet <Jessycharlet@gmail.com>
2024-04-03 15:56:56 +0200

    header 1.2
    Ajout de la barre de recherche, des animations

--- templates/header.php
+++ templates/header.php
@@ -8,21 +8,19 @@
     <link rel="stylesheet" href="./assets/css/global.css?t=<?= time(); ?>">
     <link rel="stylesheet" href="./assets/css/header.css?t=<?= time(); ?>">
 </head>
-<header class="navContainer">
-    <nav class="navLeft">
-        <button id="shop">Shop <span>></span></button>
-        <button id="infos">Infos <span>></span></button>
-    </nav>
-    <a href="./index.php" class="logo"><img src="./assets/img/logo_Games_Thrones_nav.png" alt="Logo Games Thrones" /></a>
-    <nav class="navRight">
-        <div class="searchBarContainer">
-            <div>
+
+<body>
+    <header class="navContainer">
+        <nav class="navLeft">
+            <button id="shop">Shop <span>></span></button>
+            <button id="infos">Infos <span>></span></button>
+        </nav>
+        <a href="./index.php" id="logo"><img src="./assets/img/logo_Games_Thrones_nav.png" alt="Logo Games Thrones" /></a>
+            <div id="searchBarContainer">
                 <input type="text" name="searchBar" id="searchBar">
                 <button id="searchBarClose">❌</button>
             </div>
-            <button><img src="./assets/img/icon_search.png" alt="rechercher" /></button>
-        </div>
-        <a href="<!--PAGE DU PANIER-->"><img src="./assets/img/icon_panier.png" alt="Mon panier" /><span>2</span></a>
-        <a href="<!--PAGE DE CONNEXION-->"><img src="./assets/img/icon_user.png" alt="Mon compte" /></a>
-    </nav>
-</header>
+            <button id="searchBarOpen"><img src="./assets/img/icon_search.png" alt="rechercher" /></button>
+            <a href="<!--PAGE DU PANIER-->"><img src="./assets/img/icon_panier.png" alt="Mon panier" /><span>2</span></a>
+            <a href="<!--PAGE DE CONNEXION-->"><img src="./assets/img/icon_user.png" alt="Mon compte" /></a>
+    </header>
