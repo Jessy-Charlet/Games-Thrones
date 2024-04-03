86b9a5336a5879a86713024adf50b729650d5569
Jessy Charlet <Jessycharlet@gmail.com>
2024-04-03 10:48:21 +0200

    includes

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-require_once("include.php");
+require_once("./templates/header.php");
 ?>
 <body>
     
--- templates/header.php
+++ templates/header.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Games Thrones</title>
+    <link rel="stylesheet" href="./assets/css/header.css">
 </head>
-<body>
-    
-</body>
-</html>
+<header class="menu">
+    <nav>
+dd
+    </nav>
+</header>
