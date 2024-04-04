bb4c782d72938f152c5c7acee8b96bc46bd0fd18
LordDragons <149957558+LordDragons@users.noreply.github.com>
2024-04-04 14:03:46 +0200

    footer et cgu

--- templates/footer.php
+++ templates/footer.php
@@ -1,3 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/global.css?t=<?= time(); ?>">
+    <link rel="stylesheet" href="./assets/css/header.css?t=<?= time(); ?>">
+    <title>Footer</title>
+
+</head>
+
+<footer>
+<a href="./rgpd.php"><img src= alt="rgpd" /></a>
+<a href="./contact.php"><img src= alt=" Nous contacter" /></a>
+<ul class="reseau">
+            <li class="reseau"><a href="" title="Suivez sur Facebook"
+                    target="_blank"><img class="icon" src="./imgreseau/facebook.jpg"><span aria-hidden="true"></span></a></li>
+            <li class="reseau"><a href="" title="Suivez sur Instagram"
+                    target="_blank"><img class="icon" src="imgreseau/OIP.jpg"><span aria-hidden="true"></span></a></li>
+            <li class="reseau"><a href="" title="Suivez sur LinkedIn"
+                    target="_blank"><img class="icon" src="imgreseau/linkedin.jpg"><span aria-hidden="true"></span></a></li>
+            <li class="reseau"><a href="" title="Suivez sur Twitter"
+                    target="_blank"><img class="icon" src="imgreseau/twitter.jpg"><span aria-hidden="true"></span></a></li>
+            <li class="reseau"><a href="" title="Suivez sur Youtube"
+                    target="_blank"><img class="icon" src="imgreseau/youtube.jpg"><span aria-hidden="true"></span></a></li>
+        </ul>
+<a href="./mention.php"><img src= alt="Mentions légales" /></a>
+<a href="./cgv.php"><img src= alt="CGV" /></a>
+</footer>
+
 <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
 <script src="./assets/js/searchBar.js"></script>
 </body>
+</html>
