2a48885fedc3c627c734ab8f7ec8411f2c54217e
LordDragons <149957558+LordDragons@users.noreply.github.com>
2024-04-04 15:46:26 +0200

    coucou

--- templates/footer.php
+++ templates/footer.php
@@ -9,25 +9,6 @@
 
 </head>
 
-<footer>
-<a href="./rgpd.php"><img src= alt="rgpd" /></a>
-<a href="./contact.php"><img src= alt=" Nous contacter" /></a>
-<ul class="reseau">
-            <li class="reseau"><a href="" title="Suivez sur Facebook"
-                    target="_blank"><img class="icon" src="./imgreseau/facebook.jpg"><span aria-hidden="true"></span></a></li>
-            <li class="reseau"><a href="" title="Suivez sur Instagram"
-                    target="_blank"><img class="icon" src="imgreseau/OIP.jpg"><span aria-hidden="true"></span></a></li>
-            <li class="reseau"><a href="" title="Suivez sur LinkedIn"
-                    target="_blank"><img class="icon" src="imgreseau/linkedin.jpg"><span aria-hidden="true"></span></a></li>
-            <li class="reseau"><a href="" title="Suivez sur Twitter"
-                    target="_blank"><img class="icon" src="imgreseau/twitter.jpg"><span aria-hidden="true"></span></a></li>
-            <li class="reseau"><a href="" title="Suivez sur Youtube"
-                    target="_blank"><img class="icon" src="imgreseau/youtube.jpg"><span aria-hidden="true"></span></a></li>
-        </ul>
-<a href="./mention.php"><img src= alt="Mentions légales" /></a>
-<a href="./cgv.php"><img src= alt="CGV" /></a>
-</footer>
-
 <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
 <script src="./assets/js/searchBar.js"></script>
 </body>
