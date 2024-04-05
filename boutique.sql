cd2c10359eaf770418e33fb3fba41e6d84d4819a
IsmaelLebrun <ismael.lebrun@laplateforme.io>
2024-04-05 11:05:36 +0200

    boutique sql fix

--- boutique.sql
+++ boutique.sql
@@ -94,41 +94,4 @@ ALTER TABLE
     `customer` ADD CONSTRAINT `customer_billing_address_id_foreign` FOREIGN KEY(`billing_address_id`) REFERENCES `adress`(`adress_id`);
 ALTER TABLE
     `product review` ADD CONSTRAINT `product review_product_review_id_foreign` FOREIGN KEY(`product_review_id`) REFERENCES `customer`(`customer_id`);
-    `price` DECIMAL(10, 2) NOT NULL,
-    `stock` INT NOT NULL,
-    `average_rating` DECIMAL(2, 1) NOT NULL,
-    `number_of_ratings` INT NOT NULL,
-    `vendor_code` VARCHAR(10) NOT NULL
-);
-CREATE TABLE `order_item`(
-    `order_item_id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `order_id` INT NOT NULL,
-    `product_id` INT NOT NULL,
-    `quantity` INT NOT NULL DEFAULT '1',
-    `price` DECIMAL(10, 2) NOT NULL,
-    `vendor_code` VARCHAR(10) NOT NULL
-);
-ALTER TABLE
-    `product review` ADD CONSTRAINT `product review_product_review_id_foreign` FOREIGN KEY(`product_review_id`) REFERENCES `product`(`product_id`);
-ALTER TABLE
-    `order_item` ADD CONSTRAINT `order_item_order_id_foreign` FOREIGN KEY(`order_id`) REFERENCES `order`(`order_id`);
-ALTER TABLE
-    `product` ADD CONSTRAINT `product_product_id_foreign` FOREIGN KEY(`product_id`) REFERENCES `category`(`category_id`);
-ALTER TABLE
-    `product` ADD CONSTRAINT `product_product_id_foreign` FOREIGN KEY(`product_id`) REFERENCES `brand`(`brand_id`);
-ALTER TABLE
-    `customer` ADD CONSTRAINT `customer_shipping_address_id_foreign` FOREIGN KEY(`shipping_address_id`) REFERENCES `adress`(`adress_id`);
-ALTER TABLE
-    `order` ADD CONSTRAINT `order_billing_address_id_foreign` FOREIGN KEY(`billing_address_id`) REFERENCES `adress`(`adress_id`);
-ALTER TABLE
-    `order` ADD CONSTRAINT `order_shipping_address_id_foreign` FOREIGN KEY(`shipping_address_id`) REFERENCES `adress`(`adress_id`);
-ALTER TABLE
-    `adress` ADD CONSTRAINT `adress_зostal_code_id_foreign` FOREIGN KEY(`зostal_code_id`) REFERENCES `postal code`(`зostal_code_id`);
-ALTER TABLE
-    `product image` ADD CONSTRAINT `product image_product_image_id_foreign` FOREIGN KEY(`product_image_id`) REFERENCES `product`(`product_id`);
-ALTER TABLE
-    `order` ADD CONSTRAINT `order_customer_id_foreign` FOREIGN KEY(`customer_id`) REFERENCES `customer`(`customer_id`);
-ALTER TABLE
-    `customer` ADD CONSTRAINT `customer_billing_address_id_foreign` FOREIGN KEY(`billing_address_id`) REFERENCES `adress`(`adress_id`);
-ALTER TABLE
-    `product review` ADD CONSTRAINT `product review_product_review_id_foreign` FOREIGN KEY(`product_review_id`) REFERENCES `customer`(`customer_id`);
+    
