cf025ff5f7bee9a99f77a4dd219ab11564a8d4a5
Jessy Charlet <Jessycharlet@gmail.com>
2024-04-08 16:45:05 +0200

    Update Footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,21 +1,23 @@
 </main>
 <footer>
-    <div>
+    <a href="<?= $router->generate('accueil') ?>"><img src="./assets/img/logo_Games_Thrones.png" alt="Games Thrones, le spécialiste des chaises gaming haut de gamme !" /></a>
+    <div class="footerLinks">
         <a href="<?= $router->generate('rgpd') ?>">RGPD</a>
         <a href="<?= $router->generate('contact') ?>">Nous contacter</a>
         <a href="<?= $router->generate('mention') ?>">Mentions légales</a>
         <a href="<?= $router->generate('cgv') ?>">CGV</a>
     </div>
-    <div>
-<a href="" title="Suivez nous sur Facebook" target="_blank"><img src="./assets/img/social_media_facebook.png"></a>
-<a href="" title="Suivez nous sur Instagram" target="_blank"><img src="./assets/img/social_media_instagram.png"></a>
-<a href="" title="Suivez nous sur Youtube" target="_blank"><img src="./assets/img/social_media_youtube.png"></a>
-<a href="" title="Suivez nous sur Twitter" target="_blank"><img src="./assets/img/social_media_twitter.png"></a>
-</div>
-<div>2024 - Games Thrones©</div>
+    <div class="footerSocialMedias">
+        <a href="" title="Suivez nous sur Facebook" target="_blank"><img src="./assets/img/social_media_facebook.png"></a>
+        <a href="" title="Suivez nous sur Instagram" target="_blank"><img src="./assets/img/social_media_instagram.png"></a>
+        <a href="" title="Suivez nous sur Youtube" target="_blank"><img src="./assets/img/social_media_youtube.png"></a>
+        <a href="" title="Suivez nous sur Twitter" target="_blank"><img src="./assets/img/social_media_twitter.png"></a>
+    </div>
+    <div>2024 - Games Thrones©</div>
 </footer>
 
 <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
 <script src="./assets/js/searchBar.js"></script>
 </body>
+
 </html>
