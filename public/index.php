296a5380b4053231909b345323a48188fc81caae
Alina83200 <alina.yefimova@laplateforme.io>
2024-04-18 14:35:18 +0200

    add page product.php

--- public/index.php
+++ public/index.php
@@ -11,13 +11,16 @@ $router->map('POST', '/signUpControllerphp', '../public/controller/php/signUpCon
 $router->map('POST', '/signInControllerphp', '../public/controller/php/signInController', 'signInControllerphp');
 $router->map('GET', '/profil', '/profil', 'profil');
 $router->map('GET', '/deconnexion', '../public/controller/php/deconnexion', 'deconnexion');
-$router->map('GET','/cgv','/cgv','cgv');
-$router->map('GET','/rgpd','/rgpd','rgpd');
-$router->map('GET','/mention','/mention','mention');
-$router->map('GET','/contact','/contact','contact');
-$router->map('GET','/update','../public/controller/php/updateController','updateController');
+$router->map('GET', '/cgv', '/cgv', 'cgv');
+$router->map('GET', '/rgpd', '/rgpd', 'rgpd');
+$router->map('GET', '/mention', '/mention', 'mention');
+$router->map('GET', '/contact', '/contact', 'contact');
+$router->map('GET', '/update', '../public/controller/php/updateController', 'updateController');
+$router->map('GET', '/produit', '/product', 'produit');
 
-function my_autoloader($class) {
+
+function my_autoloader($class)
+{
     include 'controller/php/classes/' . $class . '.class.php';
 }
 
@@ -26,17 +29,18 @@ spl_autoload_register('my_autoloader');
 
 $match = $router->match();
 
-if(is_array($match)){
+if (is_array($match)) {
     require '../templates/header.php';
-        if(is_callable($match['target'])){
-            call_user_func_array($match['target'], $match['params']);
-        }else{
-            $params = $match['params'];
-            require "../src/{$match['target']}.php";
-        }
+    if (is_callable($match['target'])) {
+        call_user_func_array($match['target'], $match['params']);
+    } else {
+        $params = $match['params'];
+        require "../src/{$match['target']}.php";
+    }
     require '../templates/footer.php';
-}else{
+} else {
     echo '404';
 }
 ?>
+
 </html>
