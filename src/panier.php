34f4a593263e8cf205cee5c7f94d8aafd1eeed15
LordDragons <149957558+LordDragons@users.noreply.github.com>
2024-04-19 11:03:17 +0200

    update panier.php

--- src/panier.php
+++ src/panier.php
@@ -1,19 +1,34 @@
 <?php
 
+// Fonction pour calculer le total du panier
+function calculerTotalPanier() {
+    $total = 0;
+    if(isset($_SESSION['panier'])) {
+        foreach($_SESSION['panier'] as $produit) {
+            $total += $produit['prix'] * $produit['quantite'];
+        }
+    }
+    return $total;
+}
+
 // Vérifier si le panier est vide ou non
 if(!isset($_SESSION['panier']) || empty($_SESSION['panier'])) {
     echo "Votre panier est vide.";
 } else {
-    // Afficher les produits dans le panier
-    foreach($_SESSION['panier'] as $produit) {
+    // Afficher les produits dans le panier avec les quantités
+    foreach($_SESSION['panier'] as $key => $produit) {
         echo "Nom du fauteuil: " . $produit['nom'] . "<br>";
-        echo "Prix: " . $produit['prix'] . " euros<br>";
+        echo "Prix unitaire: " . $produit['prix'] . " euros<br>";
+        echo "Quantité: <input type='number' name='quantite[$key]' value='".$produit['quantite']."' min='1'><br>";
         // Vous pouvez ajouter d'autres détails du produit ici
         echo "<hr>";
     }
 
     // Bouton pour vider le panier
-    echo "<a href='vider_panier.php'>Vider le panier</a>";
+    echo "<a href='vider_panier.php'>Vider le panier</a><br>";
+
+    // Afficher le total du panier
+    echo "Total du panier: " . calculerTotalPanier() . " euros<br>";
 
     // Autres fonctionnalités comme la validation de la commande, etc.
 }
