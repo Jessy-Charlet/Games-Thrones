f38ff5cda3d477595201268fe3cdcbc7c9d3818b
IsmaelLebrun <ismael.lebrun@laplateforme.io>
2024-04-23 09:48:38 +0200

    Correction bug

--- public/controller/php/signUpController.php
+++ public/controller/php/signUpController.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< Updated upstream
 $name = $_POST['name'];
 $firstname = $_POST['firstname'];
 $mail = $_POST['email'];
@@ -8,77 +7,8 @@ $adress = $_POST['adress'];
 $postalCode = $_POST['postalCode'];
 $city = $_POST['city'];
 $password = $_POST['password'];
-$passwordHash = password_hash($password, PASSWORD_DEFAULT);
 
-$sql = $conn->prepare("SELECT email FROM customer WHERE email = :email");
-$sql->execute(
-    array(
-        'email' => $mail
-    )
-);
-if($sql->rowCount() > 0){
-    header('Location: '.$router->generate('inscription').'?error=mailAlreadyUsed&name='.$name.'&firstname='.$firstname.'&phone='.$phone.'&adress='.$adress.'&postalCode='.$postalCode.'&city='.$city);
-    exit();
-}
-$sql->closeCursor();
-=======
-require 'classes/Database.class.php';
-require 'classes/CrudUser.class.php';
->>>>>>> Stashed changes
-
-$sql = $conn->prepare("SELECT phone FROM customer WHERE phone = :phone");
-$sql->execute(
-    array(
-        'phone' => $phone
-    )
-);
-if($sql->rowCount() > 0){
-    header('Location: '.$router->generate('inscription').'?error=phoneAlreadyUsed&name='.$name.'&firstname='.$firstname.'&mail='.$mail.'&adress='.$adress.'&postalCode='.$postalCode.'&city='.$city);
-    exit();
-}
-$sql->closeCursor();
-
-<<<<<<< Updated upstream
-try {
-    $conn->beginTransaction();
-
-    // Insertion du client
-    $insertCustomer = $conn->prepare("INSERT INTO customer (`customer_last-name`, `customer_first-name`, email, phone, password) VALUES (:customer_last_name, :customer_first_name, :email, :phone, :password)");
-    $insertCustomer->execute(
-        array(
-            'customer_last_name' => $name,
-            'customer_first_name' => $firstname,
-            'email' => $mail,
-            'phone' => $phone,
-            'password' => $passwordHash
-        )
-    );
-
-    // Récupération de l'ID du client inséré
-    $customerId = $conn->lastInsertId();
-
-    // Insertion de l'adresse
-    $insertAddress = $conn->prepare("INSERT INTO adress (city, postal_code, adress, customer_id, postal_code_id) 
-                                    SELECT :city, :postal_code, :adress, :customer_id, postal_code_id 
-                                    FROM postal_code WHERE postal_code = :postal_code");
-    $insertAddress->execute(
-        array(
-            'city' => $city,
-            'postal_code' => $postalCode,
-            'adress' => $adress,
-            'customer_id' => $customerId
-        )
-    );
-    session_start();
-    $_SESSION['user'] = $customerId;
-    header('Location: '.$router->generate('accueil'));
-    // Commit des transactions
-    $conn->commit();
-} catch(PDOException $e) {
-    // En cas d'erreur, annulation des transactions
-    $conn->rollback();
-    throw $e;
-=======
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
     $user = new CrudUser();
     $user->createUser($name, $firstname, $mail, $phone, $adress, $postalCode, $city, $password);
     session_start();
@@ -91,7 +21,6 @@ try {
             'sessionUserFirstName' => $user->getFirstname()
         )
     );
->>>>>>> Stashed changes
 }
 
 ?>
