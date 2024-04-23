9ed7ee8d98f735d78a260a972cbff2d578833f46
Alina83200 <alina.yefimova@laplateforme.io>
2024-04-23 16:38:19 +0200

    add html pour panier

--- src/panier.php
+++ src/panier.php
@@ -65,3 +65,213 @@ if(!isset($_SESSION['panier']) || empty($_SESSION['panier'])) {
     // Autres fonctionnalités comme la validation de la commande, etc.
 }
 ?>
+
+<main>
+        <div class="container">
+            <h1 class="panierTitle">Votre panier</h1>
+            <section>
+                <div class="panierContainer">
+                    <div class="panierLeft">
+                        <div class="panierItem">
+                            <div class="panierImg">
+                                <img src="./1/product_1_main-mage.jpg" alt="">
+                            </div>
+                            <div class="panierContent">
+                                <div class="panierContentLeft">
+                                    <h2 class="panierItemTitle">Nom du produit</h2>
+                                    <p>coleur</p>
+                                </div>
+                                <div class="panierContentRight">
+                                    <div class="panierItemQuantity">
+                                        <span>-</span>
+                                        <input type="text" type="namber" value="2">
+                                        <span>+</span>
+                                    </div>
+                                    <div class="panierItemSubtotal">
+                                        <p class="panierItemPrix">299.00 <span>€</span></p>
+                                        <span>
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                stroke-width="1.5" stroke="currentColor"
+                                                class="h-5 w-5 cursor-pointer duration-150 hover:text-red-500">
+                                                <path stroke-linecap="round" stroke-linejoin="round"
+                                                    d="M6 18L18 6M6 6l12 12"></path>
+                                            </svg>
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="panierItem">
+                            <div class="panierImg">
+                                <img src="./1/product_1_main-mage.jpg" alt="">
+                            </div>
+                            <div class="panierContent">
+                                <div class="panierContentLeft">
+                                    <h2 class="panierItemTitle">Nom du produit</h2>
+                                    <p>coleur</p>
+                                </div>
+                                <div class="panierContentRight">
+                                    <div class="panierItemQuantity">
+                                        <span>-</span>
+                                        <input type="text" type="namber" value="2">
+                                        <span>+</span>
+                                    </div>
+                                    <div class="panierItemSubtotal">
+                                        <p class="panierItemPrix">299.00 <span>€</span></p>
+                                        <span>
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                stroke-width="1.5" stroke="currentColor"
+                                                class="h-5 w-5 cursor-pointer duration-150 hover:text-red-500">
+                                                <path stroke-linecap="round" stroke-linejoin="round"
+                                                    d="M6 18L18 6M6 6l12 12"></path>
+                                            </svg>
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panierRight">
+                        <div class="panierTotal">
+                            <div class="panierTotalTop">
+                                <p>Total</p>
+                                <div>300.99<span>€</span></div>
+                            </div>
+                        </div>
+                        <button class="panierBtn" type="submit">Passer la comande</button>
+                    </div>
+            </section>
+            <section id="similarProducts">
+                <div>
+                    <h2 class="similarProductsTitle">Vous pourriez aussi aimer</h2>
+                    <div class="similarProductscontent">
+                        <div class="card">
+                            <div class="cardTop">
+                                <a href="#">
+                                    <img class="cardImg" src="./1/product_1_main-mage.jpg" alt="">
+                                </a>
+                            </div>
+                            <div class="cardBottom">
+                                <a href="#" class="cardTitle">Nom du produit</a>
+                                <div class="priceRating">
+                                    <div class="cardPrice cardPrice--common">436 <span>€</span></div>
+                                    <div class="rating-mini">
+                                        <span class="active"></span>
+                                        <span class="active"></span>
+                                        <span class="active"></span>
+                                        <span class="active"></span>
+                                        <span class="active"></span>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                        <div class="card">
+                            <div class="cardTop">
+                                <a href="#">
+                                    <img class="cardImg" src="./1/product_1_main-mage.jpg" alt="">
+                                </a>
+                            </div>
+                            <div class="cardBottom">
+                                <a href="#" class="cardTitle">Nom du produit</a>
+                                <div class="priceRating">
+                                    <div class="cardPrice cardPrice--common">436 <span>€</span></div>
+                                    <div class="rating-mini">
+                                        <span class="active"></span>
+                                        <span class="active"></span>
+                                        <span class="active"></span>
+                                        <span class="active"></span>
+                                        <span class="active"></span>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                        <div class="card">
+                            <div class="cardTop">
+                                <a href="#">
+                                    <img class="cardImg" src="./1/product_1_main-mage.jpg" alt="">
+                                </a>
+                            </div>
+                            <div class="cardBottom">
+                                <a href="#" class="cardTitle">Nom du produit</a>
+                                <div class="priceRating">
+                                    <div class="cardPrice cardPrice--common">436 <span>€</span></div>
+                                    <div class="rating-mini">
+                                        <span class="active"></span>
+                                        <span class="active"></span>
+                                        <span class="active"></span>
+                                        <span class="active"></span>
+                                        <span class="active"></span>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                        <div class="card">
+                            <div class="cardTop">
+                                <a href="#">
+                                    <img class="cardImg" src="./1/product_1_main-mage.jpg" alt="">
+                                </a>
+                            </div>
+                            <div class="cardBottom">
+                                <a href="#" class="cardTitle">Nom du produit</a>
+                                <div class="priceRating">
+                                    <div class="cardPrice cardPrice--common">436 <span>€</span></div>
+                                    <div class="rating-mini">
+                                        <span class="active"></span>
+                                        <span class="active"></span>
+                                        <span class="active"></span>
+                                        <span class="active"></span>
+                                        <span class="active"></span>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                        <div class="card">
+                            <div class="cardTop">
+                                <a href="#">
+                                    <img class="cardImg" src="./1/product_1_main-mage.jpg" alt="">
+                                </a>
+                            </div>
+                            <div class="cardBottom">
+                                <a href="#" class="cardTitle">Nom du produit</a>
+                                <div class="priceRating">
+                                    <div class="cardPrice cardPrice--common">436 <span>€</span></div>
+                                    <div class="rating-mini">
+                                        <span class="active"></span>
+                                        <span class="active"></span>
+                                        <span class="active"></span>
+                                        <span class="active"></span>
+                                        <span class="active"></span>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                        <div class="card">
+                            <div class="cardTop">
+                                <a href="#">
+                                    <img class="cardImg" src="./1/product_1_main-mage.jpg" alt="">
+                                </a>
+                            </div>
+                            <div class="cardBottom">
+                                <a href="#" class="cardTitle">Nom du produit</a>
+                                <div class="priceRating">
+                                    <div class="cardPrice cardPrice--common">436 <span>€</span></div>
+                                    <div class="rating-mini">
+                                        <span class="active"></span>
+                                        <span class="active"></span>
+                                        <span class="active"></span>
+                                        <span class="active"></span>
+                                        <span class="active"></span>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+            </section>
+        </div>
+        </div>
+    </main>
