7846f5c37b828a52bd7bcd293c4fc5dd0586cae8
Alina83200 <alina.yefimova@laplateforme.io>
2024-04-25 10:10:10 +0200

    Move script src

--- templates/footer.php
+++ templates/footer.php
@@ -1,6 +1,7 @@
 </main>
 <footer>
-    <a href="<?= $router->generate('accueil') ?>"><img src="./assets/img/logo_Games_Thrones.png" alt="Games Thrones, le spécialiste des chaises gaming haut de gamme !" /></a>
+    <a href="<?= $router->generate('accueil') ?>"><img src="./assets/img/logo_Games_Thrones.png"
+            alt="Games Thrones, le spécialiste des chaises gaming haut de gamme !" /></a>
     <div class="footerLinks">
         <a href="<?= $router->generate('rgpd') ?>">RGPD</a>
         <a href="<?= $router->generate('contact') ?>">Nous contacter</a>
@@ -8,8 +9,10 @@
         <a href="<?= $router->generate('cgv') ?>">CGV</a>
     </div>
     <div class="footerSocialMedias">
-        <a href="" title="Suivez nous sur Facebook" target="_blank"><img src="./assets/img/social_media_facebook.png"></a>
-        <a href="" title="Suivez nous sur Instagram" target="_blank"><img src="./assets/img/social_media_instagram.png"></a>
+        <a href="" title="Suivez nous sur Facebook" target="_blank"><img
+                src="./assets/img/social_media_facebook.png"></a>
+        <a href="" title="Suivez nous sur Instagram" target="_blank"><img
+                src="./assets/img/social_media_instagram.png"></a>
         <a href="" title="Suivez nous sur Youtube" target="_blank"><img src="./assets/img/social_media_youtube.png"></a>
         <a href="" title="Suivez nous sur Twitter" target="_blank"><img src="./assets/img/social_media_twitter.png"></a>
     </div>
@@ -18,6 +21,7 @@
 
 <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
 <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"></script>
 <script src="./assets/js/searchBar.js"></script>
 <script src="./assets/js/banner.js"></script>
 </body>
