8bbe5e05f840f3bb4adcbed0f8de675a04c06662
Jessy Charlet <Jessycharlet@gmail.com>
2024-04-25 10:52:50 +0200

    add section new products
    Ajout de la section nouveaux produits sur la page d'accueil

--- src/home.php
+++ src/home.php
@@ -1,17 +1,37 @@
 <?php
 include '../templates/banner.php';
-?>
-<h2>Les nouveautés<h2>
-<section class="productsGrid">
-<?php
-$conn = Database::connect();
-$sql = $conn->prepare("SELECT * FROM product");
-$sql->execute();
-$result = $sql->fetch(PDO::FETCH_ASSOC);
-foreach ($result as $res){
-    echo '<p><pre>' . print_r($res) .'</pre></p>';
-
+function showProducts($result, $number)
+{
+    foreach ($result as $prod) {
+        if ($number > 0){
+            $img = json_decode($prod['images'], true);
+            echo "<div class='product'>
+            <a href='/produit?id=" . $prod['product_id'] . "'>
+            <img src='" . $img['main_image'] . "' alt='" . $prod['name'] . "'/>
+            <div class='productName'>" . $prod['name'] . "</div>
+            <div class='productDiv'>
+            <div class='productPrice'>" . $prod['price'] . " €</div>
+            <div class='productRate'>" . $prod['average_rating'] . "<img src='./assets/img/star.png'/></div>
+            </div>
+            </a>
+            </div>";
+            $number--;
+        }
+    }
 }
 ?>
+<section class="section">
+    <h2>Nos derniers Thrones</h2>
+            <div class="productsGrid">
+                <?php
+                $conn = Database::connect();
+                $sql = $conn->prepare("SELECT * FROM product ORDER BY product_id DESC");
+                $sql->execute();
+                $result = $sql->fetchAll(PDO::FETCH_ASSOC);
+                showProducts($result, 8);
+                ?>
+            </div>
 </section>
+<section class="section">
 <h2>Les promotions</h2>
+</section>
