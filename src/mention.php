c5e6e5556cd3e4a1f4554b2d42245e3e0aba4fbe
LordDragons <149957558+LordDragons@users.noreply.github.com>
2024-05-14 13:58:09 +0200

    Update mention.php
    Correction mineur

--- src/mention.php
+++ src/mention.php
@@ -17,7 +17,7 @@ Numéro de téléphone : [Numéro de téléphone de l'hébergeur]</p>
 <p>Le présent site et l'ensemble de son contenu (textes, images, vidéos, etc.) sont la propriété de [Votre entreprise] ou de tiers ayant autorisé [Votre entreprise] à les utiliser. Toute reproduction, représentation, modification, publication, transmission ou dénaturation, totale ou partielle, du site ou de son contenu, par quelque procédé que ce soit, et sur quelque support que ce soit, est interdite.</p>
 
 <h2>Protection des Données Personnelles</h2>
-<p>Les informations recueillies sur ce site sont enregistrées dans un fichier informatisé par [Votre entreprise] pour la gestion de la clientèle et des prospects. Elles sont conservées pendant [durée de conservation] et sont destinées à [service destinataire]. Conformément à la loi « informatique et libertés », vous pouvez exercer votre droit d'accès aux données vous concernant et les faire rectifier en contactant : [adresse de contact].</p>
+<p>Les informations recueillies sur ce site sont enregistrées dans un fichier informatisé par [Votre entreprise] pour la gestion de la clientèle et des prospects. Elles sont conservées pendant [durée de conservation] et sont destinées à [service destinataire]. Conformément à la loi « informatique et libertés », vous pouvez exercer votre droit d'accès aux données vous concernant et les faire rectifier en  nous <a href="<?= $router->generate('contact') ?>">contactant</a>.</p>
 
 <h2>Liens Hypertextes</h2>
 <p>Le site peut contenir des liens hypertextes vers d'autres sites internet. La responsabilité de [Votre entreprise] ne saurait être engagée quant au contenu de ces sites ni aux dommages pouvant résulter de leur utilisation.</p>
@@ -26,5 +26,5 @@ Numéro de téléphone : [Numéro de téléphone de l'hébergeur]</p>
 <p>[Votre entreprise] s'efforce de mettre à jour le contenu du site et de fournir des informations précises. Cependant, [Votre entreprise] ne peut garantir l'exactitude, la complétude et l'actualité des informations diffusées sur le site.</p>
 
 <h2>Contact</h2>
-<p>Si vous avez des questions concernant ces CGU, veuillez nous contacter à l'aide du formulaire que vous trouverez ici : <a href="<?= $router->generate('contact') ?>">Nous contacter</a>.</p>
+<p>Si vous avez des questions concernant ces Mentions Légales, veuillez nous contacter à l'aide du formulaire que vous trouverez ici : <a href="<?= $router->generate('contact') ?>">Nous contacter</a>.</p>
 </section>
