d7b4847dc76cce67081985feb3e6b36e40bff3b7
LordDragons <149957558+LordDragons@users.noreply.github.com>
2024-05-14 10:10:06 +0200

    Update product.php
    Ajout du fil d'ariane en php

--- src/product.php
+++ src/product.php
@@ -13,6 +13,34 @@ function sliderPhotos($images, $product)
 ?>
 
 <section class="section">
+<?php
+$def = "index";
+$dPath = $_SERVER['REQUEST_URI'];
+$dChunks = explode("/", $dPath);
+
+echo('<a class="dynNav" href="/">Accueil</a><span class="dynNav"> > </span>');
+for($i=1; $i<count($dChunks); $i++ ){
+    echo('<a class="dynNav" href="/');
+    for($j=1; $j<=$i; $j++ ){
+        echo($dChunks[$j]);
+        if($j!=count($dChunks)-1){ echo("/");}
+    }
+
+    if($i==count($dChunks)-1){
+        $prChunks = explode(".", $dChunks[$i]);
+        if ($prChunks[0] == $def) $prChunks[0] = "";
+        $prChunks[0] = $prChunks[0] . '</a>';
+    }
+    else $prChunks[0]=$dChunks[$i] . '</a><span class="dynNav"> > </span>';
+    echo('">');
+    if ($i == count($dChunks) - 1 && isset($product)) {
+        echo(str_replace("_" , " " , $product["name"]));
+    } else {
+        echo(str_replace("_" , " " , $prChunks[0]));
+    }
+}
+?>
+
     <section id="description">
         <div class="container">
             <div class="cartForm">
